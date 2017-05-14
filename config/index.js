66474ea49fcafbc045b4dace99e239c99032cf56
rajesh krishnan <getmetorajesh@gmail.com>
2017-05-14 23:21:53 +1000

    refactored slackclient

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 
 module.exports = {
-  witToken: process.env.WIT_API_TOKEN,
-  slackToken: process.env.SLACK_BOT_TOKEN
-}
+    witToken: process.env.WIT_API_TOKEN,
+    slackToken: process.env.SLACK_BOT_TOKEN,
+    slackLogLevel: 'verbose'
+};
