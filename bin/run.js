abf9c566e09a3d5ce53301dbfe44fb991f2ef57d
rajesh krishnan <getmetorajesh@gmail.com>
2017-05-14 10:51:57 +1000

    add config

--- bin/run.js
+++ bin/run.js
@@ -1,14 +1,15 @@
 'use strict';
 
+const config = require('../config');
 const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const witToken = 'WIT_API_TOKEN';
+const witToken = config.witToken;
 const witClient = require('../server/witClient')(witToken);
 
-const slackToken = 'SLACK_API_TOKEN';
+const slackToken = config.slackToken;
 const slackLogLevel = 'verbose';
 
 const serviceRegistry = service.get('serviceRegistry');
@@ -18,5 +19,5 @@ rtm.start();
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
 server.on('listening', function() {
-    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+  console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
 });
---
+++ config/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+
+module.exports = {
+  witToken: process.env.WIT_API_TOKEN,
+  slackToken: process.env.SLACK_BOT_TOKEN
+}
